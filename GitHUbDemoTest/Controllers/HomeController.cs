17a037f61a9a14595879000e07d8d37a8c455c1f
amitsargar <amitsargar2153@gmail.com>
2016-04-03 13:03:59 +0530

    Test

--- GitHUbDemoTest/Controllers/HomeController.cs
+++ GitHUbDemoTest/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace GitHUbDemoTest.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View(); 
         }
 
         public ActionResult About()
